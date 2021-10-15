6e1c0bd0ab5a6f715e9d792ec8aa5748c89dac5b
sebastian.nee@student.supsi.ch <sebastianwilliamnee@gmail.com>
2021-10-15 11:01:55 +0200

    added player model, added basic look around

---
+++ Assets/Scripts/RespawnCollider.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnCollider : MonoBehaviour
+{
+    public Transform RespawnPoint;
+    
+    //Detect collisions between the GameObjects with Colliders attached
+    void OnTriggerEnter(Collider other)
+    {
+        //Check for a match with the specific tag on any GameObject that collides with your GameObject
+        if (other.gameObject.tag == "Player")
+        {
+            other.transform.position = RespawnPoint.position;
+            other.transform.rotation = Quaternion.identity;
+        }
+    }
+}
